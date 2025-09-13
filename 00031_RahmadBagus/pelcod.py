51ecc7ee9a9f54f1ef9c80dcd8569214a7a14aaf
tyoo-18 <bukanbowo123@gmail.com>
2025-09-13 11:29:47 +0700

    00031_RahmaBagus

---
+++ 00031_RahmadBagus/pelcod.py
@@ -0,0 +1,48 @@
+print("="*50)
+print("assalamualaikum")
+print("="*50)
+
+nama = "Rahmad Bagus Setiawan"
+print("nama saya adalah",nama)
+
+print("="*50)
+
+print(type(nama))
+
+print("="*50)
+
+boolean = True 
+print(type(boolean))
+
+print("="*50)
+
+penjumlahan = 5 + 7
+print("hasilnya adalah",penjumlahan)
+
+print("="*50)
+
+Nama_dosen = "isi sendiri"
+b = Nama_dosen
+print(b)
+
+# print("="*50)
+
+# TahunLahir = int(input("masukkan tahun lahir "))
+# TahunSekarang = int(input("masukkan tahun sekarang "))
+# HitungUsia = TahunSekarang - TahunLahir
+# print("usianya adalah ",HitungUsia)
+
+print("="*50)
+
+angka1 = 45
+angka2 = 7
+cek = angka1 % angka2
+print(cek)
+
+print("="*50)
+
+saldo = float(input("Masukkan Saldo : "))
+deposit = float(input("Masukkan saldo deposit : "))
+
+saldo += deposit
+print(saldo)
